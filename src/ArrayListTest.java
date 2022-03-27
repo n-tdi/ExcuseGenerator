19ddbcd4e7be82d76829c456339cf3574c82c933
Ntdi <79174456+professional-tdi@users.noreply.github.com>
2022-03-27 07:52:41 -0400

    Update ArrayListTest.java

--- src/ArrayListTest.java
+++ src/ArrayListTest.java
@@ -1,16 +1,18 @@
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.File;
+import java.io.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class ArrayListTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ArrayList<String> listData = new ArrayList<String>();
         Gson gson = new Gson();
-        File jsonFile = new File("src/data.json");
-        //listData.add(gson.fromJson(jsonFile, "intro"));
-
+        FileReader reader = new FileReader("src/data.json");
+        String json = String.valueOf(reader.read());
+        String data = gson.fromJson(json, String.class);
+        listData.add(data);
+        System.out.println(listData);
     }
 }
